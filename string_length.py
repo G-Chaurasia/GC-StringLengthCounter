8e2b9d412a4962ecbb8c1717e6f1b9e0cb4a93d5
G. Chaurasia <sunflowerlearning9@gmail.com>
2025-09-26 20:38:11 +0530

    Add string length counter snippet

---
+++ string_length.py
@@ -0,0 +1,9 @@
+# Program to count length of a string using len()
+
+# take input from user
+text = input("Enter a string: ")
+
+# use len() to find length
+length = len(text)
+
+print("Length of the string is:", length)
